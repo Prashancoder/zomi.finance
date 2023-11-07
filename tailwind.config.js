1db7215d03ebc98a3d2de9664d7df7ab6fa6bc8a
Prashancoder <78917743+Prashancoder@users.noreply.github.com>
2023-11-07 06:51:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      colors:
+      { 
+       transparent: 'transparent',
+       current: 'currentColor', 
+        
+       'razo': {
+         400: '#04032D',
+       }
+ 
+     },
+
+
+    },
+  },
+  plugins: [],
+}
+
+
+
+
+// '#04032D'
